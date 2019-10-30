e4dfdc57c4c036a49c4b7f3208116029638f7f5b
Rozen Raymond <zen.revilene@gmail.com>
2019-10-30 23:25:50 +1100

    WIP

--- src/components/Resources/ResourceCard.js
+++ src/components/Resources/ResourceCard.js
@@ -1,28 +1,26 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { red } from "@material-ui/core/colors";
+import { red, blue } from "@material-ui/core/colors";
 import clsx from "clsx";
 import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
-import Avatar from "@material-ui/core/Avatar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import Collapse from "@material-ui/core/Collapse";
 import Rating from "@material-ui/lab/Rating";
 import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 import Button from "@material-ui/core/Button";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import BookmarkIcon from "@material-ui/icons/Bookmark";
 import ShareIcon from "@material-ui/icons/Share";
 import LinkIcon from "@material-ui/icons/Link";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
+import Chip from "@material-ui/core/Chip";
 
 const useStyles = makeStyles(theme => ({
   card: {
-    maxWidth: 345
+    width: 345
   },
   media: {
     height: 0,
@@ -40,35 +38,68 @@ const useStyles = makeStyles(theme => ({
   },
   avatar: {
     backgroundColor: red[500]
+  },
+  highlight: {
+    backgroundColor: blue[100],
+    borderRadius: "3px",
+    padding: "6px",
+    margin: "3px 6px 3px 0"
+  },
+  mediaWrapper: {
+    display: "flex"
   }
 }));
 
-export const ResourceCard = ({ title, created, description, url }) => {
+export const ResourceCard = ({
+  title,
+  created,
+  url,
+  paid,
+  media_type,
+  tags
+}) => {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+
+  const source = new URL(url).host;
+  const createdYear = new Date(created).getFullYear();
+
   return (
-    <Card class={classes.card}>
-      <CardHeader
-        avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={title}
-        subheader={created}
-      />
+    <Card className={classes.card}>
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {description}
+        <Typography variant="h5" color="textSecondary">
+          {title}
+        </Typography>
+
+        <Typography variant="body1" color="textSecondary" component="p">
+          {source} | Last updated {createdYear}
+        </Typography>
+
+        <div className={classes.mediaWrapper}>
+          <div className={classes.highlight}>{paid ? "Paid" : "Free"}</div>
+          <div className={classes.highlight}>{media_type}</div>
+        </div>
+
+        <div>
+          {tags.map(tag => {
+            return <Chip label={tag.name} />;
+          })}
+        </div>
+
+        <Typography variant="body1" color="textSecondary" component="p">
+          Learning Notes:
+        </Typography>
+
+        <Typography variant="body1" color="textSecondary" component="p">
+          Hangout Proposed:
+        </Typography>
+
+        <Typography variant="body1" color="textSecondary" component="p">
+          Questions | Resolved
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
